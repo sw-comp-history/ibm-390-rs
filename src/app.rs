3d16753f47ad2b161602707a96cb139bc7c19cb1
Mike Wright <mike@softwarewrighter.com>
2026-04-08 12:21:01 -0700

    add Blog, Discord, YouTube links to footer

--- src/app.rs
+++ src/app.rs
@@ -600,6 +600,9 @@ pub fn app() -> Html {
                 <div class="footer-left">
                     <span>{"MIT License"}</span>
                     <span>{"© 2026 Michael A Wright"}</span>
+                    <a href="https://software-wrighter-lab.github.io/" target="_blank">{"Blog"}</a>
+                    <a href="https://discord.com/invite/Ctzk5uHggZ" target="_blank">{"Discord"}</a>
+                    <a href="https://www.youtube.com/@SoftwareWrighter" target="_blank">{"YouTube"}</a>
                 </div>
                 <div class="footer-right">
                     <span>{format!("{} | {} | {}", env!("VERGEN_BUILD_HOST"), env!("VERGEN_GIT_SHA_SHORT"), env!("VERGEN_BUILD_TIMESTAMP"))}</span>
